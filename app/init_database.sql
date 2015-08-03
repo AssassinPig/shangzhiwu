fccf5311f3b49f24ac66aea40a929be8a174c34e
root <root@iZ28iytfx1mZ.(none)>
2015-08-03 09:23:05 +0800

    add fcgi

---
+++ app/init_database.sql
@@ -0,0 +1,9 @@
+ALTER TABLE metainfo CONVERT TO CHARACTER SET utf8;
+ALTER TABLE branch CONVERT TO CHARACTER SET utf8;
+ALTER TABLE signup CONVERT TO CHARACTER SET utf8;
+ALTER TABLE basicinfo CONVERT TO CHARACTER SET utf8;
+ALTER TABLE user CONVERT TO CHARACTER SET utf8;
+
+insert into user values(1, 'admin', 'abc@gmail.com', '27149', '18616991234' )
+
+insert into basicinfo values(1, 'shangzhiwu', 'introduction介绍内容', '/www.abc.com/pic' )
---
+++ debug.py
@@ -0,0 +1,3 @@
+from app import app
+if __name__ == "__main__":
+	app.run(debug = True, host='120.27.50.82')
